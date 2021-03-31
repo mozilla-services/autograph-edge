c172bf42b0f7b1a1d1036ddd8106fe19285dfb90
Greg Guthe <gguthe@mozilla.com>
2021-03-31 14:32:28 -0400

    fix go vet error for context string type
    'should not use basic type string as key in context.WithValue'

--- context.go
+++ context.go
@@ -5,10 +5,19 @@ import (
 	"net/http"
 )
 
-const contextKeyRequestID = "reqID"
+type contextKey struct {
+	name string
+}
+
+func (k *contextKey) String() string { return "github.com/mozilla-services/autograph-edge context value " + k.name }
+
+var (
+	// ctxReqID is the string identifier of a request ID in a context
+	contextKeyRequestID = contextKey{name: "reqID"}
+)
 
 // addToContext add the given key value pair to the given request's context
-func addToContext(r *http.Request, key string, value interface{}) *http.Request {
+func addToContext(r *http.Request, key contextKey, value interface{}) *http.Request {
 	ctx := r.Context()
 	return r.WithContext(context.WithValue(ctx, key, value))
 }
