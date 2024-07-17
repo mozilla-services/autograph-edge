fc2b98081d22794d49035a3977e9af81a149b01c
Ben Hearsum <ben@mozilla.com>
2024-07-17 14:47:44 -0400

    temp: store signed.apk as an artifact to help debug intermittent issues

--- integration_test/sign_test_apk.sh
+++ integration_test/sign_test_apk.sh
@@ -9,6 +9,6 @@ while ! nc -z "$host" "$port"; do
   echo -n "."
   sleep 1 # wait for a sec before checking again
 done
-curl -F "input=@test.apk" -o /tmp/signed.apk \
+curl -F "input=@test.apk" -o /app/signed.apk \
   -H "Authorization: dd095f88adbf7bdfa18b06e23e83896107d7e0f969f7415830028fa2c1ccf9fd" \
   "http://$host:$port/sign"
